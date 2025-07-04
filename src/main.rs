54686167474b42bd34c17ee4d28bc86c76363a6c
kx0101 <liakos.koulaxis@yahoo.com>
2025-07-04 10:32:45 +0300

    adsf

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Could not bind to address");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+
+    let reponse = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}",);
+
+    stream
+        .write_all(reponse.as_bytes())
+        .expect("Failed to write response");
+}
